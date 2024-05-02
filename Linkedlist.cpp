2cc2f432f93d60d6068174aa168f2e1d6af3aff9
MalkingMan <arrayalkhozini@gmail.com>
2024-05-02 15:56:23 +0700

    commit 3

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -19,5 +19,27 @@ void addNode() {
     cin >> newNode->noMhs;      // Assign value to the data field of the new node
     cout << "\nEnter the name of the student:";
     cin >> newNode->name;       // Assign value to the data field of the new node
+    
+    // Insert the new node in the list
+    if (START == NULL || newNode->noMhs <= START->noMhs)
+    {
+
+        if (START != NULL && newNode->noMhs == START->noMhs)
+        {
+            cout << "\033[31mDuplicate roll numbers not allowed\033]0m" <<endl;
+            return;
+        }
+        // if the list is empty, make the new node the start
+        newNode->next = START; // step 3: make the new node point to the first node
+        
+        
+    }
+
+    {
+        /* code */
+    }
+    
 
 }
+
+
