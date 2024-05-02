c5060819ca6576add99970c4feed04ddb87bca39
MalkingMan <arrayalkhozini@gmail.com>
2024-05-02 16:01:11 +0700

    commit 4

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -31,7 +31,13 @@ void addNode() {
         }
         // if the list is empty, make the new node the start
         newNode->next = START; // step 3: make the new node point to the first node
-        
+
+        if (START != NULL) 
+        {
+            START->prev = newNode; // step 4: make the first node point to the new node
+        }
+        newNode->prev = NULL;      // step 5 : make the node point to NULL
+        START = newNode; // step 6: make the new node the first node
         
     }
 
